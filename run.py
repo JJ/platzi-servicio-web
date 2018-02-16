8c8e34e173cab4f9097cf281c8768f1339d9272a
JJ Merelo <jjmerelo@gmail.com>
2018-02-16 12:44:22 +0100

    Devuelve json

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, Response
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/status')
+def status():
+    return jsonify({ 'status': 'OK'})

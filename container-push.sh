e8be364a562ba1e6aadaf44945452e1374e0e44c
JJ Merelo <jjmerelo@gmail.com>
2018-02-18 10:32:53 +0100

    Añade imagen al registro

---
+++ container-push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Hay que hacer login en el registro
+# az acr login --name jjplatzi
+docker tag jjmerelo/platzi-hitos jjplatzi.azurecr.io/hitos:v1
+docker push jjplatzi.azurecr.io/hitos:v1
+az container create --name platzicg -g platzi --image jjplatzi.azurecr.io/hitos
+
--- container.sh
+++ container.sh
@@ -4,4 +4,4 @@
 az group create -l westeurope -n platzi
 az acr create --resource-group platzi --name jjplatzi --sku Basic
 az acr list --resource-group platzi --query "[].{acrLoginServer:loginServer}" --output table
-#az vm container create -g platzi -n pltzjj --dns-name-prefix jj-pltz
+

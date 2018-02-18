01340ebd5c80998bc206e1479f714ce2d8f64c8f
JJ Merelo <jjmerelo@gmail.com>
2018-02-18 11:57:07 +0100

    Medio funcionando

--- image-push.sh
+++ image-push.sh
@@ -5,4 +5,4 @@
 docker tag jjmerelo/platzi-hitos jjplatzi.azurecr.io/hitos:v1
 docker push jjplatzi.azurecr.io/hitos:v1
 az container create --name platzicg -g platzi --image jjplatzi.azurecr.io/hitos
-
+az acr repository list --name jjplatzi --output table

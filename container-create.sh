eb7a5aaa67d8a16f68f425edeb0e93cd6fedd13c
JJ Merelo <jjmerelo@gmail.com>
2018-02-18 12:26:54 +0100

    Contenedor creado

--- container-create.sh
+++ container-create.sh
@@ -2,6 +2,6 @@
 
 # Hay que hacer login en el registro
 # Se usa con la clave en una variable de entorno AZ_CR_KEY
-# export AZ_CR_KEY=$(az acr credential show --name jjplatzi --query "passwords[0].value")
+# export AZ_CR_KEY=$(az acr credential show --name jjplatzi --query "passwords[0].value" --out tsv)
 az acr repository list --name jjplatzi --output table # Comprueba que existe
-az container create --resource-group platzi --name jjplatzi --image hitos:v1 --cpu 1 --memory 1 --registry-login-server jjplatzi.azurecr.io --registry-username jjplatzi --registry-password $AZ_CR_KEY --ip-address public --ports 80
+az container create -g platzi --name hitosv1 --image jjplatzi.azurecr.io/hitos:v1 --registry-password $AZ_CR_KEY

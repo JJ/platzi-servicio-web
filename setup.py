e310d642e5496b5ceed291baec3cbd0f6438432b
JJ Merelo <jjmerelo@gmail.com>
2018-02-19 16:49:22 +0100

    Añade opción "run"
    Gracias a @juanlu001 por el consejo.

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
 from setuptools import setup, find_packages
+import distutils.cmd
+import subprocess
+
+class GunicornCommand(distutils.cmd.Command):
+  """A custom command to run Pylint on all Python source files."""
+
+  description = 'ejecuta gunicorn'
+  user_options = [
+  ]
+
+  def initialize_options(self):
+      pass 
+
+  def finalize_options(self):
+      pass
+  
+  def run(self):
+    """Run command."""
+    command = ['/home/jmerelo/.pyenv/shims/gunicorn -b 0.0.0.0:8000 hitor:app']
+    self.announce(
+        'Running command: %s' % str(command),
+        level=distutils.log.INFO)
+    subprocess.check_call(command, shell=True)
 
 setup(
     name='hitor',
@@ -12,5 +35,8 @@ setup(
         'pytest',
     ],
     test_suite = 'tests',
+    cmdclass={
+        'run': GunicornCommand,
+    },
     
 )

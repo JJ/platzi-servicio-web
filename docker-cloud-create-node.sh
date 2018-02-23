54e20e3c5c51857ec7106d0a0791703a6bb6aaa3
JJ Merelo <jjmerelo@gmail.com>
2018-02-23 18:46:53 +0100

    Nodos más grandes

--- docker-cloud-create-node.sh
+++ docker-cloud-create-node.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Primero levantar un nodo
-docker-cloud nodecluster create -t 1 --tag platzi platzi-node digitalocean fra1 512mb
+docker-cloud nodecluster create -t 1 --tag platzi platzi-big digitalocean fra1 2gb
+
 # docker-cloud nodecluster rm platzi-node
 # Este nodo está en Frankfurt (fra1) y tiene el tamaño mínimo (512mb)
 # Se destruye con docker-cloud nodecluster rm platzi-node
